7790e36598337bf76a22c963b32558a4cf37d11c
Dahaka934 <dahaka934@gmail.com>
2021-01-06 16:41:33 +0300

    Added possibility to use polytypic objects in vector functions

--- include/glsl/vector_functions.h
+++ include/glsl/vector_functions.h
@@ -8,28 +8,17 @@ namespace glsl {
 
 inline constexpr double pi = std::numbers::pi_v<double>;
 
-#define NARGS(...) \
-    NARGS_(0, ##__VA_ARGS__, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
-#define NARGS_(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, N, ...) N
-
-#define GLUE(x, y) GLUE_(x, y)
-#define GLUE_(x, y) x##y
-
-#define FUN_ARGS(type, ...) GLUE(FUN_ARGS, NARGS(__VA_ARGS__))(type, __VA_ARGS__)
-#define FUN_ARGS1(type, x) type x
-#define FUN_ARGS2(type, x, ...) FUN_ARGS1(type, x), FUN_ARGS1(type, __VA_ARGS__)
-#define FUN_ARGS3(type, x, ...) FUN_ARGS1(type, x), FUN_ARGS2(type, __VA_ARGS__)
-
-#define DEF_VEC_FUNC(func, impl, ...)                           \
-template<class T>                                               \
-constexpr auto func(FUN_ARGS(const T&, __VA_ARGS__)) {          \
-    if constexpr (concepts::Vector<T>) {                        \
-        constexpr auto fun = func<traits::vector_item_t<T>>;    \
-        return details::apply(fun, __VA_ARGS__);                \
-    } else {                                                    \
-        return T(impl(__VA_ARGS__));                            \
-    }                                                           \
-}                                                               \
+#define DEF_VEC_FUNC(func, impl)                                    \
+template<class... Args>                                             \
+constexpr auto func(const Args& ...args) {                          \
+    using T = typename traits::first<Args...>::type;                \
+    if constexpr (concepts::Vector<T>) {                            \
+        constexpr auto fun = func<traits::vector_item_t<Args>...>;  \
+        return details::apply(fun, args...);                        \
+    } else {                                                        \
+        return T(impl(args...));                                    \
+    }                                                               \
+}                                                                   \
 
 namespace details {
 
@@ -129,46 +118,37 @@ constexpr T lessThanEqual(T x, T y) {
 
 } // namespace details
 
-DEF_VEC_FUNC(acos, std::acos, rad)
-DEF_VEC_FUNC(asin, std::asin, rad)
-DEF_VEC_FUNC(cos, std::cos, rad)
-DEF_VEC_FUNC(sin, std::sin, rad)
-DEF_VEC_FUNC(tan, std::tan, rad)
-DEF_VEC_FUNC(degrees, details::degrees, rad)
-DEF_VEC_FUNC(radians, details::radians, deg)
-DEF_VEC_FUNC(abs, std::abs, x)
-DEF_VEC_FUNC(ceil, std::ceil, x)
-DEF_VEC_FUNC(exp, std::exp, x)
-DEF_VEC_FUNC(exp2, std::exp2, x)
-DEF_VEC_FUNC(floor, std::floor, x)
-DEF_VEC_FUNC(fract, details::fract, x)
-DEF_VEC_FUNC(isinf, std::isinf, x)
-DEF_VEC_FUNC(isnan, std::isnan, x)
-DEF_VEC_FUNC(log, std::log, x)
-DEF_VEC_FUNC(log2, std::log2, x)
-DEF_VEC_FUNC(max, std::max, x, y)
-DEF_VEC_FUNC(min, std::min, x, y)
-DEF_VEC_FUNC(clamp, std::clamp, x, min, max)
-DEF_VEC_FUNC(mod, details::mod, x, y)
-DEF_VEC_FUNC(pow, std::pow, x, y)
-DEF_VEC_FUNC(round, std::round, x)
-DEF_VEC_FUNC(sign, details::sign, x)
-DEF_VEC_FUNC(smoothstep, details::smoothstep, edge0, edge1, x)
-DEF_VEC_FUNC(sqrt, std::sqrt, x)
-DEF_VEC_FUNC(inversesqrt, details::inversesqrt, x)
-DEF_VEC_FUNC(step, details::step, edge, x)
-DEF_VEC_FUNC(trunc, std::trunc, x)
-DEF_VEC_FUNC(faceforward, details::faceforward, n, i, nref)
-
-template<class T, class B>
-constexpr auto mix(const T& x, const T& y, const B& a) {
-    if constexpr (concepts::Vector<T>) {
-        constexpr auto fun = mix<traits::vector_item_t<T>, traits::vector_item_t<B>>;
-        return details::apply(fun, x, y, a);
-    } else {
-        return details::mix(x, y, a);
-    }
-}
+DEF_VEC_FUNC(acos, std::acos)
+DEF_VEC_FUNC(asin, std::asin)
+DEF_VEC_FUNC(cos, std::cos)
+DEF_VEC_FUNC(sin, std::sin)
+DEF_VEC_FUNC(tan, std::tan)
+DEF_VEC_FUNC(degrees, details::degrees)
+DEF_VEC_FUNC(radians, details::radians)
+DEF_VEC_FUNC(abs, std::abs)
+DEF_VEC_FUNC(ceil, std::ceil)
+DEF_VEC_FUNC(exp, std::exp)
+DEF_VEC_FUNC(exp2, std::exp2)
+DEF_VEC_FUNC(floor, std::floor)
+DEF_VEC_FUNC(fract, details::fract)
+DEF_VEC_FUNC(isinf, std::isinf)
+DEF_VEC_FUNC(isnan, std::isnan)
+DEF_VEC_FUNC(log, std::log)
+DEF_VEC_FUNC(log2, std::log2)
+DEF_VEC_FUNC(max, std::max)
+DEF_VEC_FUNC(min, std::min)
+DEF_VEC_FUNC(clamp, std::clamp)
+DEF_VEC_FUNC(mod, details::mod)
+DEF_VEC_FUNC(pow, std::pow)
+DEF_VEC_FUNC(round, std::round)
+DEF_VEC_FUNC(sign, details::sign)
+DEF_VEC_FUNC(smoothstep, details::smoothstep)
+DEF_VEC_FUNC(sqrt, std::sqrt)
+DEF_VEC_FUNC(inversesqrt, details::inversesqrt)
+DEF_VEC_FUNC(step, details::step)
+DEF_VEC_FUNC(trunc, std::trunc)
+DEF_VEC_FUNC(faceforward, details::faceforward)
+DEF_VEC_FUNC(mix, details::mix)
 
 template<concepts::Vector T>
 constexpr auto mix(const T& x, const T& y) {
@@ -237,21 +217,13 @@ constexpr bool any(const T& x) {
     }
 }
 
-DEF_VEC_FUNC(equal, details::equal, x, y)
-DEF_VEC_FUNC(notEqual, details::notEqual, x, y)
-DEF_VEC_FUNC(greaterThan, details::greaterThan, x, y)
-DEF_VEC_FUNC(greaterThanEqual, details::greaterThanEqual, x, y)
-DEF_VEC_FUNC(lessThan, details::lessThan, x, y)
-DEF_VEC_FUNC(lessThanEqual, details::lessThanEqual, x, y)
+DEF_VEC_FUNC(equal, details::equal)
+DEF_VEC_FUNC(notEqual, details::notEqual)
+DEF_VEC_FUNC(greaterThan, details::greaterThan)
+DEF_VEC_FUNC(greaterThanEqual, details::greaterThanEqual)
+DEF_VEC_FUNC(lessThan, details::lessThan)
+DEF_VEC_FUNC(lessThanEqual, details::lessThanEqual)
 
-#undef NARGS
-#undef NARGS_
-#undef GLUE
-#undef GLUE_
-#undef FUN_ARGS
-#undef FUN_ARGS1
-#undef FUN_ARGS2
-#undef FUN_ARGS3
 #undef DEF_VEC_FUNC
 
 } // namespace glsl
